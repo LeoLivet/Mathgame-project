d41c345d453f11b101bc6e514debbc547b96b080
LeoLivet <CharlyLLarsen@gmail.com>
2024-10-29 18:28:21 +0100

    More building done

--- Mathgame project/Program.cs
+++ Mathgame project/Program.cs
@@ -2,7 +2,10 @@
 //You should record previous games in a List and there should be an option in the 
 //menu for the user to visualize a history of previous games.
 
-Random random = new Random();
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.Arm;
+
+Random rnd = new Random();
 
 int points = 0;
 int healthPoints = 0;
@@ -10,22 +13,14 @@ int healthPoints = 0;
 int lowNumber = 0;
 int highNumber = 20;
 
-string input = "";
 bool validInput = false;
-
 WelcomeMessage();
-ChooseMenu(Console.ReadLine();)
 
 
-input = Console.ReadLine();
 
-do
-{
-	while (input == "")
-	{
+ChooseMenu();
 
-	}
-} while (validInput == false);
+
 
 
 void WelcomeMessage()
@@ -33,55 +28,84 @@ void WelcomeMessage()
 	Console.WriteLine("Welcome to the Math game");
 	Console.WriteLine("Press enter to start!");
 }
-// void AddToHistory()//put nogle overrides ind her
-// {
-// 	// lav en array her som tager input fra console og sætter det ind her.
-// }
 
-void ChooseMenu(int input) 
+
+void ChooseMenu()
 {
-	switch (input)
+
+
+
+	string input = Console.ReadLine();
+
+	int result = int.Parse(input);
+
+
+	switch (result)
 	{
 		case 1: //+
-		Console.WriteLine("You have chosen Additon!");
-		// TODO add option to press enter to continue or e(?) to go back (like a method)
-		AdditionGame();
-		break;
-		
+			Console.WriteLine("You have chosen Additon!");
+
+			AdditionGame();
+			break;
+
 		case 2: //-
-		Console.WriteLine("You have chosen Subtraction!");
-		break;
-		
+			Console.WriteLine("You have chosen Subtraction!");
+			int intA = rnd.Next(50);
+			int intB = rnd.Next(50);
+
+			break;
+
 		case 3: // *
-		Console.WriteLine("You have chosen Multiplicaton!");
-		break;
-		
+			Console.WriteLine("You have chosen Multiplicaton!");
+			break;
+
 		case 4: // /
-		Console.WriteLine("You have chosen Division!");
-		break;
-		
+			Console.WriteLine("You have chosen Division!");
+
+			break;
+
 		case 5: //Se tidligere resultater
-		
-		
+
+			break;
+
 		default:
-		Console.WriteLine("please only pick between the specified choices");
-		break;
+			Console.WriteLine("please only pick between the specified choices");
+			break;
 	}
 }
 
 void AdditionGame()
 {
+	int intA = rnd.Next(50);
+	int intB = rnd.Next(50);
+	int correctAnswer = intA + intB;
+	Console.WriteLine($"What is {intA} + {intB}?");
+	int answer = GetAnswer();
 
+
+	for (int i = 0; i < 4; i++)
+	{
+
+		if (answer == correctAnswer)
+		{
+			System.Console.WriteLine("Congrats thats correct!");
+		}
+		else
+		{
+			System.Console.WriteLine("Incorrect 100 points from gryffindor!");
+
+		}
+	}
 }
 
 void SubtractionGame()
 {
-	
+
 }
 
 void MultiplicatonGame()
 {
-	
+
 }
 
 void DivisionGame()
@@ -95,4 +119,10 @@ Generate a random first number (n1)
 Generate a random multiplier (m)
 Use the product of the first number and the multiplier as the second number (n2 = n1 * m)
 */
+}
+
+int GetAnswer()
+{
+	int input = Convert.ToInt32(Console.ReadLine());
+	return input;
 }
